576b58406b4624cf8e963a3d97645335d552d97a
Kacper Wach <wach.kacper8@gmail.com>
2025-03-08 12:54:43 +0100

    Dodano opcję losowego wstawiania kwadratów

---
+++ dodajLosowo.m
@@ -0,0 +1,18 @@
+function [out] = dodajLosowo(img)
+
+[w,k] = size(img);
+out = img;
+ile = input("Ile chcesz wprowadzic przeszkod? ");
+
+for i=1:ile
+        
+    x0 = randi([1,k]);
+    y0 = randi([1 w]);
+    a = randi([1 20]);
+     out = insertShape(out,'filled-rectangle',[x0,y0,a,a],'Color', 'black', 'Opacity', 1);
+     out = rgb2gray(out);
+    
+  
+
+end
+
--- main.m
+++ main.m
@@ -12,7 +12,7 @@ clc
 
 
 
-jakiObraz = input("Jesli chcesz wybrac mape wpisz 1, jesli dodac wlasne przeszkody wybierz 2: ");
+jakiObraz = input("Jesli chcesz wybrać mapę wpisz 1, jeśli dodać własne przeszkody wpisz 2, jeśli chcesz wybrać losowa ilość przeszkód wpisz 3 ");
 
 switch jakiObraz
     case 1
@@ -22,21 +22,24 @@ switch jakiObraz
     case 2
         in = imread('map.png');
         img = dodajPrzeszkody(in);
+    case 3
+        in = imread('map.png');
+        img = dodajLosowo(in);
     otherwise
-        error('Podano bledny numer')
+        error('Podano błędny numer')
         
 
 end
 
-metoda = input("Ktora metode chcesz wybrac? \n RRT wpisz 1, PRM wpisz 2: ");
- xs = input("Podaj wspolrzedna x punktu poczatkowego ");
- ys = input("Podaj wspolrzedna y punktu poczatkowego ");
- kierunekS = input("Podaj obrot punktu poczatkowego ");
+metoda = input("Którą metode chcesz wybrać? \n RRT wpisz 1, PRM wpisz 2 ");
+ xs = input("Podaj wspólrzędną x punktu początkowego ");
+ ys = input("Podaj współrzędną y punktu początkowego ");
+ kierunekS = input("Podaj obrót punktu początkowego ");
  start = [xs,ys, kierunekS];
 
- xg = input("Podaj wspolrzedna x punktu koncowego: ");
- yg = input("Podaj wspolrzedna y punktu koncowego: ");
- kierunekG = input("Podaj obrot punktu  koncowego: ");
+ xg = input("Podaj współrzędną x punktu końcowego ");
+ yg = input("Podaj współrzędną y punktu końcowego ");
+ kierunekG = input("Podaj obrót punktu  końcowego ");
  goal = [xg,yg, kierunekG];
 
 switch metoda
