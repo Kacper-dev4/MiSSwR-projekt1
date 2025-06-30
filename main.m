c728006c1ed8968774bddc9949e00a53f9fd77ba
Kacper Wach <wach.kacper8@gmail.com>
2025-06-30 18:39:41 +0200

    Końcowa wersja najpewniej

--- main.m
+++ main.m
@@ -1,8 +1,8 @@
 clear all
 clc
 
-start = [12.0,68.0,0]; % Zygzak
-goal = [57.0,44.0,0];
+% start = [12.0,68.0,0]; % Zygzak
+% goal = [57.0,44.0,0];
 
 %start = [28.0,20.0,0]; % N
 %goal = [70.0,78.0,0];
@@ -10,7 +10,8 @@ goal = [57.0,44.0,0];
 % start = [34.0,168.0,0]; % Dywan
 % goal = [174.0,10.0,0];
 
-
+% start = [1.0,1.0,0.0];
+% goal = [100.0,100.0,0.0];
 
 jakiObraz = input("Jesli chcesz wybrać mapę wpisz 1, jeśli dodać własne przeszkody wpisz 2, jeśli chcesz wybrać losowa ilość przeszkód wpisz 3 ");
 
@@ -29,21 +30,23 @@ switch jakiObraz
         imwrite(img,'wlasnaLosowe.png');
     otherwise
         error('Podano błędny numer')
-        
+
 
 end
 
-metoda = input("Którą metode chcesz wybrać? \n RRT wpisz 1, PRM wpisz 2 ");
- %xs = input("Podaj wspólrzędną x punktu początkowego ");
- %ys = input("Podaj współrzędną y punktu początkowego ");
- %kierunekS = input("Podaj obrót punktu początkowego ");
- %start = [xs,ys, kierunekS];
 
- %xg = input("Podaj współrzędną x punktu końcowego ");
- %yg = input("Podaj współrzędną y punktu końcowego ");
- %kierunekG = input("Podaj obrót punktu  końcowego ");
+
+ metoda = input("Którą metode chcesz wybrać? \n RRT wpisz 1, PRM wpisz 2 ");
+ xs = input("Podaj wspólrzędną x punktu początkowego ");
+ ys = input("Podaj współrzędną y punktu początkowego ");
+ kierunekS = input("Podaj obrót punktu początkowego ");
+ start = [xs,ys, kierunekS];
+
+ xg = input("Podaj współrzędną x punktu końcowego ");
+ yg = input("Podaj współrzędną y punktu końcowego ");
+ kierunekG = input("Podaj obrót punktu  końcowego ");
  %kierunekG = 0;
- %goal = [xg,yg, kierunekG];
+ goal = [xg,yg, kierunekG];
 
 switch metoda
     case 1
